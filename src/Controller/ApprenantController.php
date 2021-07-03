a59f54e33b52d932cdbd331beb4d2164d346037a
saliou-t <zalltalla46@gmail.com>
2021-07-03 15:19:10 +0000

    ajout du formulaire pour les apprenants

--- src/Controller/ApprenantController.php
+++ src/Controller/ApprenantController.php
@@ -3,20 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\Apprenant;
+use App\Entity\Categorie;
 use App\Repository\ApprenantRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApprenantController extends AbstractController
 {
-    #[Route('/apprenant', name: 'apprenant')]
-    public function index( ApprenantRepository $Apprenantrepo): Response
+    #[Route('/', name: 'apprenant')]
+    public function index(ApprenantRepository $Apprenantrepo): Response
     {
         $apprenants = $Apprenantrepo->findAll();
 
         return $this->render('apprenant/index.html.twig', [
             'apprenant' => $apprenants
         ]);
+    }
+
+    #[Route('/addapprenant', name: 'addApprenant')]
+    public function form( Request $request, EntityManagerInterface $manager){
+                
+        $apprenant = new Apprenant();
+        //création du formulaire
+        $form = $this->createFormBuilder($apprenant)
+                     ->add('nom', TextType::class)
+                     ->add('prenom', TextType::class)
+                     ->add('niveau_etude', TextType::class)
+                     ->add('telephone', TextType::class)
+                     ->add('attentes', TextareaType::class)
+                     ->getForm();
+
+            return $this->render('apprenant/addApprenant.html.twig', [
+                'formApp' => $form->createView()
+            ]);
+        
     }
 }
