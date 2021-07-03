5d930d5bfbb40d4d180a5e2375bdd18990ab2ea5
saliou-t <zalltalla46@gmail.com>
2021-07-03 13:16:27 +0000

    fonctionalité d'affichage de la liste des apprenants

--- src/Controller/ApprenantController.php
+++ src/Controller/ApprenantController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Apprenant;
+use App\Repository\ApprenantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApprenantController extends AbstractController
 {
     #[Route('/apprenant', name: 'apprenant')]
-    public function index(): Response
+    public function index( ApprenantRepository $Apprenantrepo): Response
     {
+        $apprenants = $Apprenantrepo->findAll();
+
         return $this->render('apprenant/index.html.twig', [
-            'controller_name' => 'ApprenantController',
+            'apprenant' => $apprenants
         ]);
     }
 }
